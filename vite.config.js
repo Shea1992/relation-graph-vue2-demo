51e2f60668d93782dec2012d5e7b8f12c2f13f87
Shea1992 <shea_java@163.com>
2025-02-14 18:13:21 +0800

    Update vite.config.js
    github page

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,17 @@ import { createVuePlugin } from 'vite-plugin-vue2';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
+   base: './',
   console.log('Vite run:', command);
   return {
     plugins: [
-      createVuePlugin()
+      createVuePlugin(),
+       vue()
     ].filter(Boolean),
     resolve: {
       extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
       alias: {
+         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     }
   };
